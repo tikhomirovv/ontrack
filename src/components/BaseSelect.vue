f018b7bacc69355ba31986c149b2c9248639e9d7
Valerii Tikhomirov <v.tikhomirov.dev@gmail.com>
2023-09-03 16:04:31 +0300

    BaseSelect & BaseButton components

---
+++ src/components/BaseSelect.vue
@@ -0,0 +1,23 @@
+<script setup>
+defineProps(['selected', 'options', 'placeholder'])
+import BaseButton from './BaseButton.vue'
+import { XMarkIcon } from '@heroicons/vue/24/outline'
+</script>
+<template>
+  <div class="flex gap-2">
+    <BaseButton>
+      <XMarkIcon class="h-8" />
+    </BaseButton>
+    <select class="w-full truncate rounded bg-gray-100 px-2 py-1 text-2xl">
+      <option selected disabled value="">{{ placeholder }}</option>
+      <option
+        v-for="{ value, label } in options"
+        :key="value"
+        :value="value"
+        :selected="value === selected"
+      >
+        {{ label }}
+      </option>
+    </select>
+  </div>
+</template>
--- src/components/TimelineItem.vue
+++ src/components/TimelineItem.vue
@@ -1,6 +1,5 @@
 <script setup>
-import { XMarkIcon } from '@heroicons/vue/24/outline'
-
+import BaseSelect from './BaseSelect.vue'
 const props = defineProps(['timelineItem'])
 const hourLinkClasses = [
   'absolute -top-4 left-1/2 -translate-x-1/2 rounded  px-2 font-mono text-lg',
@@ -14,22 +13,15 @@ const options = [
   { value: 2, label: 'Reading' },
   { value: 3, label: 'Training' },
 ]
+const selectedActivityId = 1
 </script>
 <template>
   <li class="relative flex flex-col gap-2 border-t border-gray-200 px-4 py-10">
     <a href="#" :class="hourLinkClasses"> {{ timelineItem.hour }}:00 </a>
-    <div class="flex gap-2">
-      <button
-        class="rounded bg-gray-100 p-3 enabled:hover:bg-gray-200 disabled:cursor-not-allowed disabled:opacity-50"
-      >
-        <XMarkIcon class="h-8" />
-      </button>
-      <select class="w-full truncate rounded bg-gray-100 px-2 py-1 text-2xl">
-        <option selected disabled value="">Rest</option>
-        <option v-for="{ value, label } in options" :key="value" :value="value">
-          {{ label }}
-        </option>
-      </select>
-    </div>
+    <BaseSelect
+      :selected="selectedActivityId"
+      :options="options"
+      placeholder="Rest"
+    />
   </li>
 </template>
